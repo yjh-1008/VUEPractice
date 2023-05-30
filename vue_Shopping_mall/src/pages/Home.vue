bfc4c58a9743c506fed79b1d6202b1c20a8ffc19
yjh1008 <exesea12@gmail.com>
2023-05-30 23:59:25 +0900

    1. api연결

--- vue_Shopping_mall/src/pages/Home.vue
+++ vue_Shopping_mall/src/pages/Home.vue
@@ -21,47 +21,8 @@
     <div class="album py-5 bg-body-tertiary">
       <div class="container">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg
-                class="bd-placeholder-img card-img-top"
-                width="100%"
-                height="225"
-                xmlns="http://www.w3.org/2000/svg"
-                role="img"
-                aria-label="Placeholder: Thumbnail"
-                preserveAspectRatio="xMidYMid slice"
-                focusable="false"
-              >
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c" />
-                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-              </svg>
-              <div class="card-body">
-                <p class="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button
-                      type="button"
-                      class="btn btn-sm btn-outline-secondary"
-                    >
-                      View
-                    </button>
-                    <button
-                      type="button"
-                      class="btn btn-sm btn-outline-secondary"
-                    >
-                      Edit
-                    </button>
-                  </div>
-                  <small class="text-body-secondary">9 mins</small>
-                </div>
-              </div>
-            </div>
+          <div class="col" v-for="(item,idx) in state.items" :key="idx">
+            <Card/>
           </div>
         </div>
       </div>
@@ -73,12 +34,19 @@
 
 <script>
 import axios from 'axios'
+import Card from '@/components/Card.vue'
+import {reactive} from 'vue'
 export default {
   name: "Home",
+  components: {Card},
   setup() {
-    axios.get('/api/items').then((res)=> {
-      console.log(res)
+    const state = reactive({
+      items:[],
     })
+    axios.get('/api/items').then((res)=> {
+      state.items = res.data
+    })
+    return state
   }
 };
 </script>
